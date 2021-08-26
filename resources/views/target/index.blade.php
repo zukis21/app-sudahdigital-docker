dcf98546eb8ba2ceec961b99059dd8df3ab9f9bb
Zuki Setiawan <setyawanzuky@gmail.com>
2021-08-26 12:33:55 +0700

    rev code list target sales

--- resources/views/target/index.blade.php
+++ resources/views/target/index.blade.php
@@ -34,57 +34,57 @@
 			<tbody>
 				<?php $no=0;?>
 				@foreach($targets as $u)
-				<?php $no++;?>
-				<tr>
-					<!--<td>{{$no}}</td>-->
-					<td>
-						{{$u->users->name}}
-					</td>
-					<td>
-						@php
-							$customer = \App\Customer::where('user_id',$u->user_id)->count();
-						@endphp
-						{{$customer.' Customers'}}
-					</td>
-					<td>
-						{{number_format($u->target_values)}}
-					</td>
-					<td>
-						@php
-							$month= date('m', strtotime($u->period));
-							$year= date('Y', strtotime($u->period));
-							//dd($month);
-							$order_ach = \App\Order::where('user_id',$u->user_id)
-									->whereNotNull('customer_id')
-									->whereMonth('created_at', '=', $month)
-									->whereYear('created_at', '=', $year)
-									->where('status','!=','CANCEL')->get();
-									$total_ach = 0;
-                              			foreach($order_ach as $p){
-                                 			 $total_ach += $p->total_price;
-                              			}
-                              		//return $total_ach;
-                              		echo number_format($total_ach);
-						@endphp
-						<!--{{number_format($u->target_achievement)}}-->
-					</td>
-					<td>
-						{{date('M-Y', strtotime($u->period))}}
-					</td>
-					<td>
-						{{$u->created_by ? $u->created_of->name : ''}}
-						<br>
-						{{$u->created_by ? $u->created_at : ''}}
-					</td>
-					<td>
-						{{$u->updated_by ? $u->updated_of->name : ''}}
-						<br>
-						{{$u->updated_by ? $u->updated_at : ''}}
-					</td>
-					<td>
-						<a class="btn btn-info btn-xs" href="{{route('sales.edit_target',[$vendor,Crypt::encrypt($u->id)])}}"><i class="material-icons">edit</i></a>&nbsp;
-					</td>
-				</tr>
+					<?php $no++;?>
+					<tr>
+						<!--<td>{{$no}}</td>-->
+						<td>
+							{{$u->users ? $u->users->name : ''}}
+						</td>
+						<td>
+							@php
+								$customer = \App\Customer::where('user_id',$u->user_id)->count();
+							@endphp
+							{{$customer.' Customers'}}
+						</td>
+						<td>
+							{{number_format($u->target_values)}}
+						</td>
+						<td>
+							@php
+								$month= date('m', strtotime($u->period));
+								$year= date('Y', strtotime($u->period));
+								//dd($month);
+								$order_ach = \App\Order::where('user_id',$u->user_id)
+										->whereNotNull('customer_id')
+										->whereMonth('created_at', '=', $month)
+										->whereYear('created_at', '=', $year)
+										->where('status','!=','CANCEL')->get();
+										$total_ach = 0;
+											foreach($order_ach as $p){
+												$total_ach += $p->total_price;
+											}
+										//return $total_ach;
+										echo number_format($total_ach);
+							@endphp
+							<!--{{number_format($u->target_achievement)}}-->
+						</td>
+						<td>
+							{{date('M-Y', strtotime($u->period))}}
+						</td>
+						<td>
+							{{$u->created_by ? $u->created_of->name : ''}}
+							<br>
+							{{$u->created_by ? $u->created_at : ''}}
+						</td>
+						<td>
+							{{$u->updated_by ? $u->updated_of->name : ''}}
+							<br>
+							{{$u->updated_by ? $u->updated_at : ''}}
+						</td>
+						<td>
+							<a class="btn btn-info btn-xs" href="{{route('sales.edit_target',[$vendor,Crypt::encrypt($u->id)])}}"><i class="material-icons">edit</i></a>&nbsp;
+						</td>
+					</tr>
 				@endforeach
 			</tbody>
 		</table>
